f3467896a47905c0c43f936b929c08943fee1c3d
Kebogile <kebogile.porogo@vodacom.co.za>
2023-10-13 00:40:51 +0200

    modify api to Retrieve and filter/sort brands

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,11 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Brand = require('../models/Brand');
 
-// Retrieve all brands (example)
+// Retrieve and filter/sort brands
 router.get('/brands', async (req, res) => {
+  // Your existing code to retrieve and filter/sort brands
+});
+
+// Add a new brand to the database
+router.post('/brands', async (req, res) => {
   try {
-    const brands = await Brand.find({ published: true }).sort('name');
-    res.json(brands);
+    const { name, category, published } = req.body; // Assuming you send this data in the request body
+
+    // Create a new brand
+    const newBrand = new Brand({ name, category, published });
+    await newBrand.save();
+
+    res.status(201).json(newBrand); // Respond with the newly created brand
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
