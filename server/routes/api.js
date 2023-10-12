a95420162dc6bbf8832c9e6ba8ba05ac9ba821ad
Kebogile <kebogile.porogo@vodacom.co.za>
2023-10-13 00:39:43 +0200

    Define api routes for retrieving brands

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,21 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Brand = require('../models/Brand');
 
-// Retrieve and filter/sort brands
+// Retrieve all brands (example)
 router.get('/brands', async (req, res) => {
-  // Your existing code to retrieve and filter/sort brands
-});
-
-// Add a new brand to the database
-router.post('/brands', async (req, res) => {
   try {
-    const { name, category, published } = req.body; // Assuming you send this data in the request body
-
-    // Create a new brand
-    const newBrand = new Brand({ name, category, published });
-    await newBrand.save();
-
-    res.status(201).json(newBrand); // Respond with the newly created brand
+    const brands = await Brand.find({ published: true }).sort('name');
+    res.json(brands);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
