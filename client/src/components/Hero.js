1f92ce6f961ec4a58de191441112aa11842768a1
Kebogile <kebogile.porogo@vodacom.co.za>
2023-10-14 00:28:07 +0200

    client side FE components

---
+++ client/src/components/Hero.js
@@ -0,0 +1,8 @@
+// Hero.js
+import React from 'react';
+
+const Hero = () => {
+  // Hero component content...
+};
+
+export default Hero;
---
+++ client/src/components/WhatWeDo.js
@@ -0,0 +1,8 @@
+// WhatWeDo.js
+import React from 'react';
+
+const WhatWeDo = () => {
+  // "What We Do" component content...
+};
+
+export default WhatWeDo;
