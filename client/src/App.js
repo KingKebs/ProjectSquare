31e45eae2585f640fbe1d8fbe475eb20c52ebd33
Kebogile <kebogile.porogo@vodacom.co.za>
2023-10-17 08:47:49 +0200

    client side, , Hero,css, CaseStudySlider

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+// App.js
+import React from 'react';
+import Menu from './components/Menu';
+import Hero from './components/Hero';
+import WhatWeDo from './components/WhatWeDo';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <Hero />
+      <WhatWeDo />
+      <CaseStudySlider />
+      {/* Other components... */}
+    </div>
+  );
+}
+
+export default App;
